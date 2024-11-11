724ff9d6f6b7632ac5a84bf0e2eaacffff9355aa
Jefferson Carlos <jeff.carlosbd@gmail.com>
2024-11-11 09:53:47 -0300

    ajusta redirecionamento

--- components/MenuDrawer.tsx
+++ components/MenuDrawer.tsx
@@ -12,67 +12,67 @@ const paths = ['dashboard', 'applications', 'logs'] as const
 type Path = typeof paths[number]
 
 interface Properties {
-    open: boolean
-    onClose: () => void
+  open: boolean
+  onClose: () => void
 }
 
 const MenuDrawer: React.FC<Properties> = (properties) => {
-    const router = useRouter()
-    const pathName = usePathname()
+  const router = useRouter()
+  const pathName = usePathname()
 
-    const t = useTranslations('components.menuDrawer')
+  const t = useTranslations('components.menuDrawer')
 
-    const menus: Record<Path, string> = {
-        dashboard: t("dashboard"),
-        applications: t('applications'),
-        logs: t("logs")
-    }
+  const menus: Record<Path, string> = {
+    dashboard: t("dashboard"),
+    applications: t('applications'),
+    logs: t("logs")
+  }
 
-    return (
-        <>
-            {properties.open && (
-                <div
-                    className="bg-black opacity-90 absolute top-0 left-0 right-0 bottom-0 z-40"
-                    onClick={() => properties.onClose()}
-                />
-            )}
-            <Card className={twMerge(
-                "w-full max-w-[240px] h-[calc(100vh-2rem)]",
-                "absolute z-50",
-                properties.open ? 'translate-x-0' : '-translate-x-[calc(100%+2.5rem)]',
-                'transition ease-in-out duration-300'
-            )} onClick={(e) => e.stopPropagation()}>
-                <CardHeader>
-                    <CardTitle className="flex justify-between">
-                        <span className={`${montserratThin.className} flex items-center`}>
-                            Auth
-                            <Zap size="24px" strokeWidth="1px" className="stroke-yellow-500 dark:stroke-yellow-300" />
-                            Thor
-                        </span>
+  return (
+    <>
+      {properties.open && (
+        <div
+          className="bg-black opacity-90 absolute top-0 left-0 right-0 bottom-0 z-40"
+          onClick={() => properties.onClose()}
+        />
+      )}
+      <Card className={twMerge(
+        "w-full max-w-[240px] h-[calc(100vh-2rem)]",
+        "absolute z-50",
+        properties.open ? 'translate-x-0' : '-translate-x-[calc(100%+2.5rem)]',
+        'transition ease-in-out duration-300'
+      )} onClick={(e) => e.stopPropagation()}>
+        <CardHeader>
+          <CardTitle className="flex justify-between">
+            <span className={`${montserratThin.className} flex items-center`}>
+              Auth
+              <Zap size="24px" strokeWidth="1px" className="stroke-yellow-500 dark:stroke-yellow-300" />
+              Thor
+            </span>
 
-                        <Button variant="outline" size="icon" onClick={() => properties.onClose()}>
-                            <X />
-                        </Button>
-                    </CardTitle>
-                </CardHeader>
-                <CardContent className="flex flex-col gap-4">
-                    {paths.map((path) => (
-                        <Button
-                            key={path}
-                            variant={pathName.includes(path) ? 'default' : 'outline'}
-                            className={pathName.includes(path) ? 'cursor-default' : 'cursor-pointer'}
-                            onClick={() => {
-                                router.push(path)
-                                properties.onClose()
-                            }}
-                        >
-                            {menus[path]}
-                        </Button>
-                    ))}
-                </CardContent>
-            </Card>
-        </>
-    )
+            <Button variant="outline" size="icon" onClick={() => properties.onClose()}>
+              <X />
+            </Button>
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="flex flex-col gap-4">
+          {paths.map((path) => (
+            <Button
+              key={path}
+              variant={pathName.includes(path) ? 'default' : 'outline'}
+              className={pathName.includes(path) ? 'cursor-default' : 'cursor-pointer'}
+              onClick={() => {
+                router.push(`/${path}`)
+                properties.onClose()
+              }}
+            >
+              {menus[path]}
+            </Button>
+          ))}
+        </CardContent>
+      </Card>
+    </>
+  )
 }
 
 export default MenuDrawer
